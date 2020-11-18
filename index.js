368935f7de1394683bbbd03ca491097bb0312769
Emil996 <emil.fetullayev.2017@mail.ru>
2020-11-18 18:05:38 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function ad(x) {
+
+	if(x = -x) {
+  document.calc.txt.value = document.calc.txt.value(Math.sign(x));
+}else {
+	document.calc.txt.value = document.calc.txt.value(Math.sign(-x));
+
+}
+
+}
